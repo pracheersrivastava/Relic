f7edf71412a9df27167cfbeb439b9fa2de08ee29
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2026-01-08 18:50:20 +0530

    feat: initialize backend with essential packages and utilities
    - Added package.json for backend with dependencies including express, mongoose, and others.
    - Created a .gitkeep file in the public/temp directory to track the folder.
    - Implemented ApiError class for standardized error handling.
    - Developed ApiResponce class for consistent API responses.
    - Added asyncHandler utility for handling asynchronous requests.

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors())  //USE is used for middleware 
+app.use(express.json({limit:'8kb'})); //setting JSON limit
+app.use(express.urlencoded({extended: true, limit:"8kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes
+
+import userRouter from "./routes/user.routes.js";
+
+
+app.use("/api/v1/users",userRouter) 
+
+//http://localhost:5000/api/v1/users/registers
+
+export { app };
---
+++ backend/index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+import connectDB from './db/DB.connect.js';
+import {app} from './app.js'
+
+dotenv.config({
+    path: './env'
+});
+
+app.on("error",(error)=>{
+            console.log("ERROR",error);
+            throw error;
+});
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
