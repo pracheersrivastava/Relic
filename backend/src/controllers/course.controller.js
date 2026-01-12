e0433e952f838c88f72f0754c95b0444f5ede0ba
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2026-01-13 01:50:37 +0530

    Add getEnrolledCourses function and route for fetching user's enrolled courses

--- backend/src/controllers/course.controller.js
+++ backend/src/controllers/course.controller.js
@@ -2,6 +2,7 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import {ApiResponce} from "../utils/ApiResponce.js";
 import {Course} from "../models/course.model.js";
+import { Enrollment } from "../models/enrollment.model.js";
 
 const getAllCourses = asyncHandler(async(req,res)=>{
     //fetch all courses from the data base
@@ -17,6 +18,37 @@ const getAllCourses = asyncHandler(async(req,res)=>{
     .json(new ApiResponce(200,courses,"All courses fetched successfully"));
 });
 
+const getEnrolledCourses = asyncHandler(async(req,res)=>{
+    //fetch all purcheshed courses of user from database
+    // return them as a list in response
+    const userId = req.user._id;
+    const courses = await Enrollment.aggregate([
+        {
+            match: {userId},
+        },
+        {
+            $lookup: {
+                from: "courses",
+                localField: "courseId",
+                foreignField: "_id",
+                as: "course"
+            }
+        },
+        { $unwind: "$course" },
+        {
+            $replaceRoot: { newRoot: "$course" }
+        }      
+    ]);
+    if (!courses.length) {
+        throw new ApiError(404, "No enrolled courses found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponce(200, courses, "Enrolled courses fetched"));
+});
+
 export {
     getAllCourses,
+    getEnrolledCourses
 }
--- backend/src/routes/courses.routes.js
+++ backend/src/routes/courses.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import {
-    getAllCourses
+    getAllCourses,
+    getEnrolledCourses
 } from "../controllers/course.controller.js";
 import {verifyJWT} from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.get("/all-courses",verifyJWT,getAllCourses);
+router.get("/my-courses",verifyJWT,getEnrolledCourses)
 
 export default router;
