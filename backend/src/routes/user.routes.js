e1342825ccf4e28d0d6e227cd71fcf8c54e8f987
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2026-01-14 22:08:49 +0530

    feat: add change password route to user routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -6,5 +6,6 @@ const router = Router();
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT,logoutUser);
+router.route("/change-password").post(verifyJWT,changeCurrentPassword);
 
 export default router;
