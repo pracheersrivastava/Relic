4b1ac965ec396a120a99e3151ad0e5ab3e5af5b0
Pracheer Srivastava <141501855+pracheersrivastava@users.noreply.github.com>
2026-01-27 23:44:12 +0530

    Add : dashboard controller

---
+++ backend/src/controllers/dashboard.controller.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponce } from "../utils/ApiResponce.js";
+
